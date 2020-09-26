83ed75e1e997a774a3bc4319ef04a470410c5490
iTh1nk <fnchao@hotmail.com>
2020-09-26 00:08:00 -0700

    setup tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ["./components/**/*.{js,ts,jsx,tsx}", "./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      screens: {
+        light: { raw: "(prefers-color-scheme: light)" },
+        dark: { raw: "(prefers-color-scheme: dark)" },
+      },
+      colors: {
+        "accent-1": "#333",
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
