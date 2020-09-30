9220e7f61f6bc0cb8abe830ae7bc0dce3770047d
iTh1nk <fnchao@hotmail.com>
2020-09-29 19:14:18 -0700

    layout change

--- layout/Container.tsx
+++ layout/Container.tsx
@@ -24,7 +24,7 @@ export const Container: React.FunctionComponent<ContainerProps> = ({
         />
       </Head>
       {/* Web Body */}
-      <div className="py-8 px-6 md:px-24">{children}</div>
+      <div className="">{children}</div>
       {/* Web Body */}
     </div>
   );
