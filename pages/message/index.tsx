eca79f52d0d24eb876e24b759585499235d698ed
iTh1nk <fnchao@hotmail.com>
2020-10-31 09:40:40 -0700

    add project props to message page

--- pages/message/index.tsx
+++ pages/message/index.tsx
@@ -17,7 +17,9 @@ const schema = yup.object().shape({
   content: yup.string().required("Content is required"),
 });
 
-interface Props {}
+interface Props {
+  dataProject: Array<Project>;
+}
 
 interface IFormInputs {
   title: string;
@@ -28,6 +30,13 @@ type MessageState = {
   isLoading: boolean;
   error: string;
   success: string;
+};
+type Project = {
+  id: string;
+  title: string;
+  author: string;
+  content: string;
+  created_at: Date;
 };
 
 const initialState: MessageState = {
@@ -70,7 +79,7 @@ function reducer(state: MessageState, action: MessageAction) {
   }
 }
 
-const AdminPostAdd: React.FunctionComponent<Props> = ({}) => {
+const AdminPostAdd: React.FunctionComponent<Props> = ({ dataProject }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
   const { isLoading, success, error } = state;
 
@@ -104,7 +113,7 @@ const AdminPostAdd: React.FunctionComponent<Props> = ({}) => {
   };
 
   return (
-    <Container>
+    <Container dataProps={dataProject?.slice(0, 3)}>
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="bg-white shadow-md px-3 pt-6 pb-8 mb-4 flex flex-col dark:bg-black">
           <div className="mb-4">
@@ -178,5 +187,27 @@ const AdminPostAdd: React.FunctionComponent<Props> = ({}) => {
     </Container>
   );
 };
+
+export async function getStaticProps() {
+  try {
+    const resProject = await fetch(
+      process.env.NEXT_PUBLIC_API + "/projects/get"
+    );
+    const dataProject = await resProject.json();
+    return {
+      props: {
+        dataProject,
+      },
+      revalidate: 1,
+    };
+  } catch (err) {
+    console.log(err);
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+}
 
 export default AdminPostAdd;
